efd6baf01c26cc312d5ab50f0787764ca2b5daea
Janyl <janyla_i@mail.ru>
2023-06-22 18:46:42 +0600

    swift

---
+++ src/test/java/git/SwiftPayment.java
@@ -0,0 +1,8 @@
+package git;
+
+public class SwiftPayment {
+
+        public void sendMoneyViaSwift(String bankName, String accountName){
+            System.out.println("Money has been send");
+    }
+}
